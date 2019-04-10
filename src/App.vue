2859ad8354026cd13715d47ad5e492af54b205b0
yutingbai <34768124+yutingbai@users.noreply.github.com>
2019-04-10 20:57:55 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
+    <ul class="footer">
+      <li class="footer-item"><router-link to="/search">搜索</router-link></li>
+      <li class="footer-item"><router-link to="/">收藏</router-link></li>
+      <li class="footer-item"><router-link to="/mine">我的</router-link></li>
+    </ul>
   </div>
 </template>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+*{
+  margin:0;
+  padding :0;
+  list-style :none;
+  text-decoration :none;
+  color:#666;
+}
+.footer{
+  display: flex;
+  background :#eee;
+  position :fixed;
+  bottom :0;
+  width 100%;
+  height :50px;
+  &-item{
+    flex-grow:1;
+    text-align:center;
+    line-height :50px;
+  }
+}
 </style>
